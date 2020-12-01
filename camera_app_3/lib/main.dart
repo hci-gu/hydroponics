7b87844d80214431d928b711f3708546ce704d5d
Sebastian Andreasson <xandse@ml-200403-001.local>
2020-12-01 11:30:49 +0100

    take picture with flash

--- camera_app_3/lib/main.dart
+++ camera_app_3/lib/main.dart
@@ -5,16 +5,12 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart' show join;
-import 'package:path_provider/path_provider.dart';
 import 'package:gallery_saver/gallery_saver.dart';
-import 'package:lamp/lamp.dart';
+import 'package:adv_camera/adv_camera.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   final cameras = await availableCameras();
-
   final firstCamera = cameras.first;
 
   runApp(
@@ -39,57 +35,40 @@ class TakePictureScreen extends StatefulWidget {
 }
 
 class TakePictureScreenState extends State<TakePictureScreen> {
-  CameraController _controller;
-  Future<void> _initializeControllerFuture;
+  AdvCameraController cameraController;
 
   @override
   void initState() {
     super.initState();
 
-    _controller = CameraController(
-      widget.camera,
-      ResolutionPreset.medium,
-    );
-
-    _initializeControllerFuture = _controller.initialize();
-
     Timer.periodic(Duration(seconds: 10), (timer) async {
-      // await Lamp.flash(new Duration(seconds: 2));
-
-      try {
-        final path = join(
-          (await getTemporaryDirectory()).path,
-          '${DateTime.now()}.png',
-        );
-
-        await _controller.takePicture(path);
-        GallerySaver.saveImage(path);
-        Uint8List bytes = File(path).readAsBytesSync();
-        await uploadImage(bytes);
-      } catch (e) {}
+      await cameraController.captureImage();
     });
   }
 
   @override
   void dispose() {
-    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0),
-
-      // Kan vara användbar när vi installerar kameran.
-
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          return CameraPreview(_controller);
-        },
-      ),
+    return AdvCamera(
+      onCameraCreated: _onCameraCreated,
+      flashType: FlashType.on,
+      bestPictureSize: true,
+      onImageCaptured: (String path) async {
+        print("onImageCaptured => " + path);
+        await GallerySaver.saveImage(path);
+        Uint8List bytes = File(path).readAsBytesSync();
+        await uploadImage(bytes);
+      },
+      cameraPreviewRatio: CameraPreviewRatio.r16_9,
     );
+  }
+
+  _onCameraCreated(AdvCameraController controller) {
+    this.cameraController = controller;
   }
 
   Future uploadImage(Uint8List imageBytes) async {
